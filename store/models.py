b075f4f8dfcee75c9a973ee856bead6f15807a6b
Thiago Willecke <thiagowillecke@MacBook-Air-de-Thiago.local>
2023-02-24 14:39:59 -0300

    replace sqlite for mysql; populate customer database

--- store/models.py
+++ store/models.py
@@ -38,7 +38,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.IntegerField()
+    phone = models.BigIntegerField()
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
