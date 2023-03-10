9d5093b7f6e109ba79c1718652a3c61931b6a8e4
Thiago Willecke <thiagowillecke@MacBook-Air-de-Thiago.local>
2023-03-10 17:00:07 -0300

    Configure e-mail backend with Smtp4dev; install/configure Redis message broker; install and configure Celery

--- storefront/settings.py
+++ storefront/settings.py
@@ -168,3 +168,15 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@thiagocode.com'
+
+ADMINS = [
+    ()
+]
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
